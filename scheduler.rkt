ce86d5d2ae7ca2b9a220b70b245c2811264bbb46
udion <uddeshya.upa@gmail.com>
2017-03-31 19:57:22 +0530

    add-to-schedule updated

--- scheduler.rkt
+++ scheduler.rkt
@@ -10,18 +10,31 @@
           #t
           #f)
       #f))
+
 (define (first-schedule-item schedule)
   (mcar schedule))
+
 (define (remove-first-schedule-item schedule)
   (begin;;if there is only one process before the removal in scheduler then like begining
     (if (null? (mcdr schedule))
         (set! schedule (mlist (mcons 0 'empty)))
         (set! schedule (mcdr schedule)))
     schedule))
+
+(define (current-time schedule)
+  ;at any point of time the process to be completed will be the
+  ;one which is at the very beginning of the list
+  ;this is due to the fact that scheduler is nothing but queue of process
+  (mcar (mcar schedule))) 
+
 (define (add-to-schedule t proc schedule)
-  (let*([new-pair (mlist (mcons t proc))]
-        [schedule (append! schedule new-pair)])
-    schedule))
+  (let*([new-pair (mlist (mcons t proc))])
+    ;if the scheduler was empty before that then the empty entry will be removed
+    (begin
+      (if (empty? schedule)
+          (set! schedule new-pair)
+          (set! schedule (append! schedule new-pair)))
+      schedule)))
 (define (append! list1 list2)
   ;;walk the first list till the end and join list2
   (define (walk l)
